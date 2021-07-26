bb162ba7dfd37b39cbff3162fb51615553ae1457
Colin Wilburn <wilburn@uchc.edu>
2021-07-26 19:30:32 -0400

    Minor tweaks after api bug fix by J Wedell

--- dep/dep_trends.py
+++ dep/dep_trends.py
@@ -29,7 +29,7 @@ with open(csr_filename, 'r') as csfile:
         csr_list[year_index] += 1
 
 csr_sub = [
-    0, 0, 0, 0, 0, 0, 0, 9, 621, 571, 676, 1195, 975, 647,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 621, 571, 676, 1195, 975, 647,
     1098, 931, 908, 658, 332
 ] # taken from https://csrosetta.bmrb.io/ ; the final number will change as 2021 progresses
 csr_totals = []
